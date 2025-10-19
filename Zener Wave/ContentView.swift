1dc272f3e97ff5d3e78da5b19f64b07c02954223
Clint Yarwood <chanandler@me.com>
2025-10-19 20:25:40 +0100

    Initial Commit

---
+++ Zener Wave/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zener Wave
+//
+//  Created by Clint Yarwood on 19/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zener Wave/Zener_WaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zener_WaveApp.swift
+//  Zener Wave
+//
+//  Created by Clint Yarwood on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Zener_WaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
